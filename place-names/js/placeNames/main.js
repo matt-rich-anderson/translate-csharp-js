dde947bbbcd3cdc784c2e25e7fafcbbc2f08a8a1
matt-rich-anderson <matt.rich.anderson@gmail.com>
2022-02-25 10:38:47 -0600

    ex 1 complete

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,12 +1,27 @@
 // Put your code here
 
 const names = [
-   "Nashville", "Hong Kong", "The back yard", "Earth",
-    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
+  "Nashville",
+  "Hong Kong",
+  "The back yard",
+  "Earth",
+  "London",
+  "The mall",
+  "Ryman Auditorium",
+  "The Great Wall of China",
 ];
 
-console.log("All Pace Names");
+console.log("All Place Names");
 
 for (const name of names) {
+  console.log(name);
+}
+
+console.log(" ");
+console.log("'The' Place Names");
+
+const theNames = names.filter((n) => n.startsWith("The"));
+
+for (const name of theNames) {
   console.log(name);
 }
