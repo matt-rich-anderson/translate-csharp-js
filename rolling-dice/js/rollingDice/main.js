a4b34620f69ae1583b64fa9c976900bf7cfa376a
matt-rich-anderson <matt.rich.anderson@gmail.com>
2022-02-25 14:27:55 -0600

    finished ex 3

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,1 +1,21 @@
-// Put your code here
+console.log("Let's roll some dice, baby!");
+console.log("---------------------------");
+
+const dieStrings = [, "one", "two", "three", "four", "five", "six"];
+
+const Roll = (min, max) => {
+  Math.ceil(min);
+  Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+for (i = 0; i < 10; i++) {
+  const die1 = Roll(1, 6);
+  const die2 = Roll(1, 6);
+
+  let message = `${dieStrings[die1]} + ${dieStrings[die2]} == ${die1 + die2}`;
+  if (die1 == die2) {
+    message += " DOUBLES!";
+  }
+  console.log(message);
+}
